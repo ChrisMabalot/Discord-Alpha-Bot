8e66ef401b580577a3235560941064abd87c68c3
ChrisMabalot <CMabalot2000@gmail.com>
2023-01-27 19:31:37 +0000

    Working on changing format of code to .command

--- bot.py
+++ bot.py
@@ -2,40 +2,61 @@ import discord
 import os
 import responses
 
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+TOKEN = os.environ.get('BOT_TOKEN')
+client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
 
-    except Exception as e:
-        print(e)
+@client.event
+async def on_ready():
+    print(f'{client.user} is now running!')
 
-def run_discord_bot():
+@client.command()
+async def hello(ctx):
+    await ctx.send("hello")
 
-    TOKEN = os.environ.get('BOT_TOKEN')
-    intents = discord.Intents.default()
-    intents.message_content = True
+@client.command(aliases=['add'])
+async def add_project(ctx):
+    await ctx.send("add project test")
 
-    client = discord.Client(intents = intents)
+client.run(TOKEN)
 
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+
+
+# async def send_message(message, user_message, is_private):
+#     try:
+#         response = responses.get_response(user_message)
+#         await message.author.send(response) if is_private else await message.channel.send(response)
+
+#     except Exception as e:
+#         print(e)
+
+# def run_discord_bot():
+
+#     TOKEN = os.environ.get('BOT_TOKEN')
+#     intents = discord.Intents.default()
+#     intents.message_content = True
+
+#     client = discord.Client(intents = intents)
+
+#     @client.event
+#     async def on_ready():
+#         print(f'{client.user} is now running!')
+
+#     @client.event
+#     async def on_message(message):
+#         if message.author == client.user:
+#             return
         
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+#         username = str(message.author)
+#         user_message = str(message.content)
+#         channel = str(message.channel)
 
-        print(f'{username} said: "{user_message}" in {channel}')
+#         print(f'{username} said: "{user_message}" in {channel}')
 
-        await send_message(message, user_message, is_private=False)
-    client.run(TOKEN)
+#         await send_message(message, user_message, is_private=False)
+#     client.run(TOKEN)
