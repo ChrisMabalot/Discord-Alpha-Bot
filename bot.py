542a52a2771cd021c5d8ad464d5ac64493a8ddda
ChrisMabalot <CMabalot2000@gmail.com>
2023-01-27 20:28:01 +0000

    Fixed typo causing code to break

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ TOKEN = os.environ.get('BOT_TOKEN')
 
 class Client(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mention_or('.'), intents=discord.Intents().all())
+        super().__init__(command_prefix=commands.when_mentioned_or('.'), intents=discord.Intents().all())
     
     async def on_ready(self):
         print(f'{client.user} is now running!')
@@ -39,40 +39,3 @@ async def embed(interaction: discord.Interaction, member:discord.Member=None):
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 client.run(TOKEN)
-
-
-
-
-# async def send_message(message, user_message, is_private):
-#     try:
-#         response = responses.get_response(user_message)
-#         await message.author.send(response) if is_private else await message.channel.send(response)
-
-#     except Exception as e:
-#         print(e)
-
-# def run_discord_bot():
-
-#     TOKEN = os.environ.get('BOT_TOKEN')
-#     intents = discord.Intents.default()
-#     intents.message_content = True
-
-#     client = discord.Client(intents = intents)
-
-#     @client.event
-#     async def on_ready():
-#         print(f'{client.user} is now running!')
-
-#     @client.event
-#     async def on_message(message):
-#         if message.author == client.user:
-#             return
-        
-#         username = str(message.author)
-#         user_message = str(message.content)
-#         channel = str(message.channel)
-
-#         print(f'{username} said: "{user_message}" in {channel}')
-
-#         await send_message(message, user_message, is_private=False)
-#     client.run(TOKEN)
