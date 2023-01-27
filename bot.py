df69932a2380f46e38814f0da7c42a0d29a0dbd3
ChrisMabalot <CMabalot2000@gmail.com>
2023-01-27 20:27:13 +0000

    Changed client to a class

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 import os
 import responses
@@ -8,31 +9,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.environ.get('BOT_TOKEN')
-client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
 
-@client.event
-async def on_ready():
-    print(f'{client.user} is now running!')
-    print('Syncing commands...')
-    synced = await client.tree.sync()
-    print(f'{str(len(synced))} commands synced successfully!')
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mention_or('.'), intents=discord.Intents().all())
+    
+    async def on_ready(self):
+        print(f'{client.user} is now running!')
+        print('Syncing commands...')
+        synced = await client.tree.sync()
+        print(f'{str(len(synced))} commands synced successfully!')
+
+client = Client()
 
 @client.tree.command(name = 'hello', description='Test Hello Command')
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(content='hello')
-    await client.close()
+    await interaction.response.send_message(content ='hello')
 
-@client.command()
-async def add(ctx):
-    await ctx.send("add project test")
+@client.tree.command(name = 'add', description = 'Add Project Command')
+async def add(interaction: discord.Interaction):
+    await interaction.response.send_message(content = 'add project test')
 
-@client.command()
-async def embed(ctx, member:discord.Member=None):
+@client.tree.command(name = 'embed', description = 'Embed Test')
+async def embed(interaction: discord.Interaction, member:discord.Member=None):
     if member == None:
-        member = ctx.message.author
-    
-    embed = discord.Embed(title='Test Embed', description = f'This is a test embed written by {member.name}', color = discord.Color.green(), timestamp = ctx.message.created_at)
-    await ctx.send(embed=embed)
+        member = interaction.user
+
+    embed = discord.Embed(title='Test Embed', description = f'This is a test embed written by {member.mention}', color = discord.Color.green(), timestamp = datetime.datetime.utcnow())
+    await interaction.response.send_message(embed=embed, ephemeral=True)
 
 client.run(TOKEN)
 
