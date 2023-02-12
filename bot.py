e6525bd5e2244fe520f98c8a07f3eb38261e09b7
ChrisMabalot <CMabalot2000@gmail.com>
2023-02-12 15:19:48 +0000

    Finished Modal class

--- bot.py
+++ bot.py
@@ -22,26 +22,22 @@ class Client(commands.Bot):
 
 class ProjectModal(ui.Modal, title = 'New Project'):
     project_name = ui.TextInput(label='Project Name', style=discord.TextStyle.short)
-    website = ui.TextInput(label='Website', style=discord.TextStyle.short)
-    discord_server = ui.TextInput(label='Discord', style=discord.TextStyle.short)
-    twitter = ui.TextInput(label='Twitter', style=discord.TextStyle.short)
-    additional_info = ui.TextInput(label='Additional Info', style=discord.TextStyle.paragraph)
+    website = ui.TextInput(label='Website', style=discord.TextStyle.short, required=False)
+    discord_server = ui.TextInput(label='Discord', style=discord.TextStyle.short, required=False)
+    twitter = ui.TextInput(label='Twitter', style=discord.TextStyle.short, required=False)
+    additional_info = ui.TextInput(label='Additional Info', style=discord.TextStyle.paragraph, required=False)
 
-    async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'Project Name: {self.project_name}\nWebsite: {self.website}')
+    async def on_submit(self, interaction: discord.Interaction, member:discord.Member=None):
+        if member == None:
+            member = interaction.user
+
+        embed = discord.Embed(title=f'{self.project_name}', description=f'Website: {self.website}\nDiscord: {self.discord_server}\nTwitter: {self.twitter}\n\nAdditional Info: {self.additional_info}', color = discord.Color.red(), timestamp = datetime.datetime.utcnow())
+        await interaction.response.send_message(embed=embed)
 
 client = Client()
 
 @client.tree.command(name = 'add', description = 'Add a new project to the channel.')
 async def add(interaction: discord.Interaction):
     await interaction.response.send_modal(ProjectModal())
-
-# @client.tree.command(name = 'embed', description = 'Embed Test')
-# async def embed(interaction: discord.Interaction, member:discord.Member=None):
-#     if member == None:
-#         member = interaction.user
-
-#     embed = discord.Embed(title='Test Embed', description = f'This is a test embed written by {member.mention}', color = discord.Color.green(), timestamp = datetime.datetime.utcnow())
-#     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 client.run(TOKEN)
